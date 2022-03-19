41deb46db9daea29af67d9a637c2d8b389d979bd
GoAlive13 <95854153+GoAlive13@users.noreply.github.com>
2022-03-19 19:38:32 +0530

    contact added

--- My_profile/src/components/Contact/Right/Right.jsx
+++ My_profile/src/components/Contact/Right/Right.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import style from "./right.module.css"
+import emailjs from "emailjs-com" 
+import { useRef } from 'react';
 
 function Right() {
+
+  const form = useRef();
+
+  function sendEmail(e)
+  {
+    e.preventDefault();
+
+    emailjs.sendForm('service_b7dlytq', 'template_i7cvnxs', form.current, '0_3On0mu-Xm1v9ZWn')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
     return (
       <div className={style.rightbody}>
         <div className={style.wrapper}>
-          <form>
-            <div>Email</div>
+          <form ref={form} onSubmit={sendEmail}>
+            <div>Name</div>
             <div>
-              <input type="text" />
+              <input type="text"  name='name' placeholder='Name' />
             </div>
             <div>Subject</div>
             <div>
-              <input type="text" />
+              <input type="text" name='subject'placeholder='Subject' />
             </div>
             <div>Message</div>
             <div>
-              <textarea name="Text1" cols="40" rows="5"></textarea>
+              <textarea name="message" cols="40" rows="5" placeholder='Your Meassage'></textarea>
             </div>
 
-            <button className={style.button}>Send</button>
+            <button type='submit' className={style.button}>Send</button>
           </form>
         </div>
       </div>
