4f3ded94933174ce74cddd08b3124a2ae8aed618
GoAlive13 <95854153+GoAlive13@users.noreply.github.com>
2022-03-23 18:21:54 +0530

    cchanges

--- My_profile/src/components/Contact/Right/Right.jsx
+++ My_profile/src/components/Contact/Right/Right.jsx
@@ -1,49 +1,64 @@
-import React from 'react'
-import style from "./right.module.css"
-import emailjs from "emailjs-com" 
-import { useRef } from 'react';
+import React from "react";
+import style from "./right.module.css";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 
 function Right() {
-
   const form = useRef();
 
-  function sendEmail(e)
-  {
+  function sendEmail(e) {
     e.preventDefault();
-
-    emailjs.sendForm('service_b7dlytq', 'template_i7cvnxs', form.current, '0_3On0mu-Xm1v9ZWn')
-      .then((result) => {
+    alert("Mail Sent..!");
+    emailjs
+      .sendForm(
+        "service_b7dlytq",
+        "template_i7cvnxs",
+        form.current,
+        "0_3On0mu-Xm1v9ZWn"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
+    form.current.reset();
   }
-    return (
-      <div className={style.rightbody}>
-        <div className={style.wrapper}>
-          <form ref={form} onSubmit={sendEmail}>
-            <div>Name</div>
-            <div>
-              <input type="text"  name='name' placeholder='Name' />
-            </div>
-            {/* <div>Email</div>
+  return (
+    <div className={style.rightbody}>
+      <div className={style.wrapper}>
+        <form ref={form} onSubmit={sendEmail}>
+          <div>Name</div>
+          <div>
+            <input type="text" name="name" placeholder="Name" />
+          </div>
+          {/* <div>Email</div>
             <div>
               <input type="email" name='email' placeholder='Email' />
             </div> */}
-            <div>Subject</div>
-            <div>
-              <input type="text" name='subject'placeholder='Subject' />
-            </div>
-            <div>Message</div>
-            <div>
-              <textarea name="message" cols="40" rows="5" placeholder='Your Message'></textarea>
-            </div>
+          <div>Subject</div>
+          <div>
+            <input type="text" name="subject" placeholder="Subject" />
+          </div>
+          <div>Message</div>
+          <div>
+            <textarea
+              name="message"
+              cols="40"
+              rows="5"
+              placeholder="Your Message"
+            ></textarea>
+          </div>
 
-            <button type='submit' className={style.button}>Send</button>
-          </form>
-        </div>
+          <button type="submit" className={style.button}>
+            Send
+          </button>
+        </form>
       </div>
-    );
+    </div>
+  );
 }
 
-export default Right
+export default Right;
