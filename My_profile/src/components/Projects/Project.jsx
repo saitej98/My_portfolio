3ef4c2edaa15c33c29d3965dba59e31e6063f507
GoAlive13 <95854153+saitej98@users.noreply.github.com>
2022-05-04 20:20:00 +0530

    project added

--- My_profile/src/components/Projects/Project.jsx
+++ My_profile/src/components/Projects/Project.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Project1 from "./Project-1/Project1";
 import Project2 from "./Project-2/Project2";
-// import Project3 from "./Project-3/Project3";
+import Project3 from "./Project-3/Project3";
 // import Project4 from "./Project-4/Project4";
 import style from "./project.module.css";
 
@@ -16,10 +16,11 @@ function Project() {
       </h1>
       <div className={style.project}>
         <div className={style.projectcontent}>
-          {/* <Project3 /> */}
+          <Project3 />
           {/* <Project4 /> */}
-          <Project1 />
           <Project2 />
+          <Project1 />
+          
         </div>
       </div>
     </>
